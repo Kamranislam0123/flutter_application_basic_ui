62968680a0a7099d7ba416b4e32dcc6477400b94
Kamranislam0123 <Kamranislam8@gmail.com>
2022-03-01 03:12:24 +0600

    Text Style

--- lib/main.dart
+++ lib/main.dart
@@ -16,81 +16,184 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Text Style',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
       ),
-      home: TextFieldExample(),
+      home: textstyle(),
     );
   }
 }
 
-class TextFieldExample extends StatefulWidget {
-  TextFieldExample({Key? key}) : super(key: key);
+class textstyle extends StatefulWidget {
+  textstyle({Key? key}) : super(key: key);
 
   @override
-  State<TextFieldExample> createState() => _TextFieldExampleState();
+  State<textstyle> createState() => _textstyleState();
 }
 
-class _TextFieldExampleState extends State<TextFieldExample> {
-  final _controllerfirstname = TextEditingController();
-  final _controllerlastname = TextEditingController();
-  String? firstName;
-  String? lastname;
+class _textstyleState extends State<textstyle> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Text Field Wxample"),
+        title: Text("Text Styling"),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(10.0),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
               child: TextField(
-                controller: _controllerfirstname,
+                decoration: InputDecoration(
+                  labelText: "Enter Email",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 30, 236, 11)),
+                      borderRadius: BorderRadius.circular(12.0)),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 11, 146, 236)),
+                      borderRadius: BorderRadius.circular(12.0)),
+                  prefixIcon: Icon(
+                    Icons.email_sharp,
+                    color: Colors.blueAccent,
+                  ),
+                  //hintText: "Email",
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+           Container(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
               child: TextField(
-                controller: _controllerlastname,
+                decoration: InputDecoration(
+                  labelText: "Enter PassWord",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 30, 236, 11)),
+                      borderRadius: BorderRadius.circular(12.0)),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 11, 146, 236)),
+                      borderRadius: BorderRadius.circular(12.0)),
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.blueAccent,
+                  ),
+                  //hintText: "Email",
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                ),
+                obscureText: true,
+                maxLength: 8,
               ),
             ),
-            Container(
+          ),
+          Container(
               padding: EdgeInsets.all(10.0),
               child: RaisedButton(
                 color: Colors.blueAccent,
                 textColor: Colors.white,
+                onPressed: () {  },
                 child: Text("Submit Now"),
-                onPressed: () {
-                  passtosecondscreen(context);
-                },
-              ),
-            ),
-            Text(
-              "FirstName:$firstName",
-              style: GoogleFonts.aclonica(
-                textStyle: TextStyle(color: Colors.blue, letterSpacing: 1.0),
-              ),
-            ),
-            Text(
-              "LastName:$lastname",
-              style: GoogleFonts.aclonica(
-                textStyle: TextStyle(color: Colors.blue, letterSpacing: .5),
-              ),
-            ),
-          ]),
+               )
+               ),
+                ],
+      ),
     );
   }
-
-  void passtosecondscreen(BuildContext context) {
-    String sendText = _controllerfirstname.text + " " + _controllerlastname.text;
-    Navigator.push(context, MaterialPageRoute(
-      builder: ((context) => secondinput(name: sendText))));
-  }
 }
+
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: TextFieldExample(),
+//     );
+//   }
+// }
+
+// class TextFieldExample extends StatefulWidget {
+//   TextFieldExample({Key? key}) : super(key: key);
+
+//   @override
+//   State<TextFieldExample> createState() => _TextFieldExampleState();
+// }
+
+// class _TextFieldExampleState extends State<TextFieldExample> {
+//   final _controllerfirstname = TextEditingController();
+//   final _controllerlastname = TextEditingController();
+//   String? firstName;
+//   String? lastname;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Text Field Wxample"),
+//       ),
+//       body: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Container(
+//               padding: EdgeInsets.all(10.0),
+//               child: TextField(
+//                 controller: _controllerfirstname,
+//               ),
+//             ),
+//             Container(
+//               padding: EdgeInsets.all(10.0),
+//               child: TextField(
+//                 controller: _controllerlastname,
+//               ),
+//             ),
+//             Container(
+//               padding: EdgeInsets.all(10.0),
+//               child: RaisedButton(
+//                 color: Colors.blueAccent,
+//                 textColor: Colors.white,
+//                 child: Text("Submit Now"),
+//                 onPressed: () {
+//                   passtosecondscreen(context);
+//                 },
+//               ),
+//             ),
+//             Text(
+//               "FirstName:$firstName",
+//               style: GoogleFonts.aclonica(
+//                 textStyle: TextStyle(color: Colors.blue, letterSpacing: 1.0),
+//               ),
+//             ),
+//             Text(
+//               "LastName:$lastname",
+//               style: GoogleFonts.aclonica(
+//                 textStyle: TextStyle(color: Colors.blue, letterSpacing: .5),
+//               ),
+//             ),
+//           ]),
+//     );
+//   }
+
+//   void passtosecondscreen(BuildContext context) {
+//     String sendText = _controllerfirstname.text + " " + _controllerlastname.text;
+//     Navigator.push(context, MaterialPageRoute(
+//       builder: ((context) => secondinput(name: sendText))));
+//   }
+// }
 
 
 
